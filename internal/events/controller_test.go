77fba570f5aecee3db450c224a1880d04b340b40
Hoeg <psh@lunar.app>
2024-03-25 08:46:34 +0100

    add basic tests

---
+++ internal/events/controller_test.go
@@ -0,0 +1,26 @@
+package events
+
+import "testing"
+
+func TestExtractCommitSha(t *testing.T) {
+	tests := []struct {
+		artifactID string
+		expected   string
+	}{
+		{
+			artifactID: "nameofdeployemnt-abcdef123456-andid",
+			expected:   "abcdef123456",
+		},
+		{
+			artifactID: "anotherdeployment-with-hyphens-123456abcdef-andid2",
+			expected:   "123456abcdef",
+		},
+	}
+
+	for _, test := range tests {
+		result := extractCommitSha(test.artifactID)
+		if result != test.expected {
+			t.Errorf("extractCommitSha(%s) = %s, expected %s", test.artifactID, result, test.expected)
+		}
+	}
+}
