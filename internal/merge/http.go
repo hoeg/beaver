aa722ed8fc8f447c56e7c93a73bec2533547c2ec
Hoeg <psh@lunar.app>
2024-03-26 07:57:32 +0100

    master branch sha

--- internal/merge/http.go
+++ internal/merge/http.go
@@ -1,13 +1,17 @@
 package merge
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/go-github/github"
 	api "github.com/hoeg/beaver/internal/generated"
 )
 
 type API struct {
+	organization string
+	client       *github.Client
 }
 
 func (m *API) PostWebhook(c *gin.Context) {
@@ -18,8 +22,21 @@ func (m *API) PostWebhook(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// process the payload and perform necessary actions
-	// ...
+	//get the sha of the branch named "master" on the repository that is referenced in the payload
+	sha, err := m.getMasterBranchSha(c.Request.Context(), *payload.Repository.FullName)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	_ = sha
 	c.JSON(http.StatusOK, gin.H{"message": "Webhook received successfully"})
+}
 
+func (a *API) getMasterBranchSha(ctx context.Context, repository string) (string, error) {
+	//get the sha of the branch named "master" on the repository that is referenced in the payload
+	branch, _, err := a.client.Repositories.GetBranch(ctx, a.organization, repository, "master")
+	if err != nil {
+		return "", err
+	}
+	return *branch.Commit.SHA, nil
 }
